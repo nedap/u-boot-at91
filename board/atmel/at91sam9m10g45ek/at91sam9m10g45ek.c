7fec619c8b0fe28dbbe543a953a50621f88837a4
Jaap de Jong <jaap.dejong@nedap.com>
2017-07-27 16:19:17 +0200

    [#149646491] Turn on caches (#12)

--- board/atmel/at91sam9m10g45ek/at91sam9m10g45ek.c
+++ board/atmel/at91sam9m10g45ek/at91sam9m10g45ek.c
@@ -411,3 +411,13 @@ void reset_phy(void)
 {
 }
 #endif
+
+void enable_caches(void)
+{
+#ifndef CONFIG_SYS_ICACHE_OFF
+	icache_enable();
+#endif
+#ifndef CONFIG_SYS_DCACHE_OFF
+	dcache_enable();
+#endif
+}
